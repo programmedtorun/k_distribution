f87ee9e698e95b6c44117260e3852b6f016ec0c5
Patrick Skelley <songkick.skelley@gmail.com>
2022-01-27 15:42:24 -0500

    this is a basic go program that uses a mysql driver to connect to a local db (password is in Patrick's lastpass app) the db it connects to is pretty basic, so this just proves the connection works, it then shoves the query results into a struct and prints them out

--- main.go
+++ main.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var pool *sql.DB // Database connection pool.
+type DThing struct {
+	DeptNo   string `json:"id"`
+	DeptName string `json:"name"`
+}
 
 func main() {
-
-	db, err := sql.Open("mysql", "root:Hellodataland123!@tcp(127.0.0.1:3306)/employees")
+	fmt.Println("What is the mysqlDB password?") // for password, reference Patrick's mysql "LastPass" app
+	var ipt string
+	fmt.Println("\033[8m")
+	fmt.Scanln(&ipt)
+	fmt.Println("\033[28m")
+	eDBAccess := "root:" + ipt + "@tcp(127.0.0.1:3306)/employees"
+	db, err := sql.Open("mysql", eDBAccess)
 	if err != nil {
-		panic(err.Error())
+		fmt.Println("There was an error opening the sql db:  ", err)
 	}
 
 	defer db.Close()
+	// Execute the query - this is a test query that has some random test data I've added to mysql on my comp
+	results, err := db.Query("SELECT * FROM employees.departments")
+	if err != nil {
+		fmt.Println("There was an error with the sql query:  ", err)
+	}
+
+	for results.Next() {
+		var dThing DThing
+		// for each row, scan the result into composite object
+		err = results.Scan(&dThing.DeptNo, &dThing.DeptName)
+		if err != nil {
+			fmt.Println("There was an error scanning data into DThing struct:  ", err)
+		}
+		// and then print out the DeptName attribute
+		fmt.Println(dThing.DeptName)
+	}
 }
